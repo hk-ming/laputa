ba40693ca99942d4d733e65f72ae0e09cad9f2e1
HyperQing <469379004@qq.com>
2017-07-03 11:13:37 +0800

    更新使用示例和composer文件

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-require_once __DIR__ .'/vendor/autoload.php';
-\hyperqing\Password::
+use hyperqing\Password;
+
+require_once __DIR__ . '/vendor/autoload.php';
+echo Password::crypt('123456')."\n";
+var_dump(Password::verify('123456', '$2y$10$9RTa6zmUkkYTVTHDkSNcU.4m8WJl/TA4eeSplFhc3ha904k/3o58u'));
