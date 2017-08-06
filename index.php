cf7ede4bfd1f39abf8c8db2266939ae62a9a4131
HyperQing <469379004@qq.com>
2017-08-06 12:22:53 +0800

    默认从环境变量中获取coding的oauth配置

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-use hyperqing\Password;
-
 require_once __DIR__ . '/vendor/autoload.php';
-echo Password::crypt('123456')."\n";
-var_dump(Password::verify('123456', '$2y$10$9RTa6zmUkkYTVTHDkSNcU.4m8WJl/TA4eeSplFhc3ha904k/3o58u'));
 
-\hyperqing\Base::response('添加成功');
+new \hyperqing\oauth\Coding();
--- src/oauth/Coding.php
+++ src/oauth/Coding.php
@@ -15,16 +15,23 @@ class Coding extends Oauth
      * 构造函数
      *
      * 实例化HTTP客户端
+     *
+     * 应用id和应用secret从环境变量中获取
+     *
+     * CODING_CLIENT_ID,CODING_CLIENT_SECRET
+     * @throws \Exception
      */
     public function __construct()
     {
-        $client_id = getenv('CODING_CLIENT_SECRET') ?? '156';
-        $client_secret = getenv('CODING_CLIENT_SECRET') ?? '879';
-        var_dump($client_secret);
-        var_dump($client_id);
+        if (!$client_id = getenv('CODING_CLIENT_ID')) {
+            throw new \Exception('OAuth: 缺少环境变量 CODING_CLIENT_ID');
+        }
+        if (!$client_secret = getenv('CODING_CLIENT_SECRET')){
+            throw new \Exception('OAuth: 缺少环境变量 CODING_CLIENT_SECRET');
+        }
         $this->config = [
             'client_id' => $client_id, // 应用id
-            'client_secret' => $client_secret, // 应用密钥（开源项目建议保存在环境变量，）
+            'client_secret' => $client_secret, // 应用密钥
             'base_uri' => 'https://coding.net', // 服务商域名
             'access_token_uri' => '/api/oauth/access_token' // 获取授权码的URI
         ];
