04e7dad62caa740c89ae734685154d6d9e679c8e
HyperQing <469379004@qq.com>
2017-08-06 11:35:10 +0800

    接受从环境变量读取配置

--- src/oauth/Coding.php
+++ src/oauth/Coding.php
@@ -13,12 +13,15 @@ class Coding extends Oauth
 {
     /**
      * 构造函数
+     *
      * 实例化HTTP客户端
-     * @param string $client_id 应用id
-     * @param string $client_secret 应用密钥
      */
-    public function __construct(string $client_id, string $client_secret)
+    public function __construct()
     {
+        $client_id = getenv('CODING_CLIENT_SECRET') ?? '156';
+        $client_secret = getenv('CODING_CLIENT_SECRET') ?? '879';
+        var_dump($client_secret);
+        var_dump($client_id);
         $this->config = [
             'client_id' => $client_id, // 应用id
             'client_secret' => $client_secret, // 应用密钥（开源项目建议保存在环境变量，）
