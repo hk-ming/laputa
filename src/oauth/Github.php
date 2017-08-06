8268f7564901b8886c59f0f5685366d044548f5b
HyperQing <469379004@qq.com>
2017-08-06 17:01:27 +0800

    接入github第三方登录

---
+++ src/oauth/Github.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace hyperqing\oauth;
+
+use hyperqing\Oauth;
+
+class Github extends Oauth
+{
+    /**
+     * 构造函数
+     *
+     * 实例化HTTP客户端
+     *
+     * 应用id和应用secret从环境变量中获取
+     *
+     * CODING_CLIENT_ID,CODING_CLIENT_SECRET
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        if (!$client_id = getenv('GITHUB_CLIENT_ID')) {
+            throw new \Exception('OAuth: 缺少环境变量 GITHUB_CLIENT_ID');
+        }
+        if (!$client_secret = getenv('GITHUB_CLIENT_SECRET')){
+            throw new \Exception('OAuth: 缺少环境变量 GITHUB_CLIENT_SECRET');
+        }
+        $this->config = [
+            'client_id' => $client_id, // 应用id
+            'client_secret' => $client_secret, // 应用密钥
+            'base_uri' => 'https://github.com', // 服务商域名
+            'access_token_uri' => '/login/oauth/access_token' // 获取授权码的URI
+        ];
+        parent::__construct();
+    }
+
+    /**
+     * 获取用户信息
+     * @return array 用户信息数组
+     */
+    public function getUser()
+    {
+        $response = $this->client->request('GET', 'https://api.github.com/user', [
+            'query' => [
+                'access_token' => $this->accessToken['access_token']
+            ]
+        ]);
+        return json_decode((string)$response->getBody(), true);
+    }
+}
