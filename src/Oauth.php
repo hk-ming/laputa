e47c26b199e9297c0478e49078d3f01eb6af384e
HyperQing <469379004@qq.com>
2017-10-30 12:57:33 +0800

    更新Oauth

--- src/Oauth.php
+++ src/Oauth.php
@@ -5,19 +5,25 @@ namespace hyperqing;
 use GuzzleHttp\Client;
 
 /**
- * 第三方登录
+ * 第三方开放平台OAuth操作父类
+ *
+ * 请继承本类后，实现getAccessToken等方法。
+ * 具体可以模仿先有的OAuth类库。
+ *
+ * 支持 HTTPS 链接。
+ *
  * @package app\index\controller
  * @author HyperQing<469379004@qq.com>
  */
 class Oauth
 {
     /**
-     * Oauth配置
+     * OAuth固定的基本信息
+     *
+     * 需要在继承子类后，在子类中覆盖该配置
      * @var array
      */
     protected $config = [
-        'client_id' => '', // 应用id
-        'client_secret' => '', // 应用密钥（开源项目建议保存在环境变量）
         'base_uri' => '', // 服务商域名
         'access_token_uri' => '' // 获取授权码的URI
     ];
@@ -25,21 +31,28 @@ class Oauth
     /**
      * 授权码数组
      *
-     * 包括:access_token,refresh_token,expires_in
+     * 包括:access_token,refresh_token,expires_in等。具体各厂商略有不同，以实际为准。<br>
+     * 请使用getAccessToken方法获取本数组
      * @var array
      */
     protected $accessToken = [];
 
+    protected $openid = [];
+
     /**
      * HTTP客户端
+     *
+     * 用于向三方平台发送请求的HTTP客户端
      * @var \GuzzleHttp\Client
      */
-    protected $client;
+    protected $client = null;
 
     /**
      * 构造函数
      *
-     * 实例化HTTP客户端
+     * 执行内容：<br>
+     * 1. 检查base_uri<br>
+     * 2. 实例化HTTP客户端
      * @throws \Exception
      */
     public function __construct()
@@ -52,31 +65,5 @@ class Oauth
             'timeout' => 10.0,
             'verify' => __DIR__ . '/oauth/cacert.pem'
         ]);
-    }
-
-    /**
-     * 获取授权码
-     *
-     * 如果未有access_token将在线获取。已经存在的则直接返回。
-     * @return array
-     */
-    public function getAccessToken()
-    {
-        if (!empty($this->accessToken)) {
-            return $this->accessToken;
-        }
-        // 取得code
-        $code = $_GET['code'];
-        // 取得授权码
-        $response = $this->client->request('GET', $this->config['access_token_uri'], [
-            'query' => [
-                'client_id' => $this->config['client_id'],
-                'client_secret' => $this->config['client_secret'],
-                'grant_type' => 'authorization_code',
-                'code' => $code
-            ]
-        ]);
-        $this->accessToken = json_decode((string)$response->getBody(), true);
-        return $this->accessToken;
     }
 }
